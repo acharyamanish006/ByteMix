e8c512089b1c84db6fdccfd9e6da2c37a53c8a20
acharyamanish006 <manish.acharya313@gmail.com>
2023-10-05 14:24:54 +1100

    class

--- Library/index.js
+++ Library/index.js
@@ -8,18 +8,18 @@
 // export default
 
 // module.exports = createWindow;
-let canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
 
 export default class ByteMix {
   constructor(width, height) {
+    this.canvas = document.createElement("canvas");
+    document.body.appendChild(this.canvas);
     this.width = width;
     this.height = height;
-    this.ctx = canvas.getContext("2d");
+    this.ctx = this.canvas.getContext("2d");
   }
   createWindow() {
-    canvas.width = this.width || window.innerWidth;
-    canvas.height = this.height || window.innerHeight;
+    this.canvas.width = this.width || window.innerWidth;
+    this.canvas.height = this.height || window.innerHeight;
   }
   createBox(x, y, width, height) {
     this.ctx.fillStyle = "red";
