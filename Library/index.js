95613045441b0920bb8fd8b4ca14cfd417be2fa8
acharyamanish006 <manish.acharya313@gmail.com>
2023-10-05 13:19:25 +1100

    basic window

--- Library/index.js
+++ Library/index.js
@@ -1,5 +1,17 @@
-function helloWorld() {
-  return "helloWorld";
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+// function helloWorld() {
+//   return "helloWorld";
+// }
+
+// module.exports = helloWorld;
+
+export default function createWindow(width, height) {
+  const ctx = canvas.getContext("2d");
+  canvas.width = width || window.innerWidth;
+  canvas.height = height || window.innerHeight;
+  ctx.fillStyle = "red"
+  ctx.fillRect(20, 20, 150, 100);
 }
 
-module.exports = helloWorld;
+// module.exports = createWindow;
